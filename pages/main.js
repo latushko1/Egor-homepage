797da867da4f235b7469dbfad8fc2c201e55e738
latushko1 <latushko1egor@gmail.com>
2022-09-14 22:21:55 +0600

    add &apos;

--- pages/main.js
+++ pages/main.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {AiOutlineArrowDown} from 'react-icons/fa'
 
 function Main() {
   return (
@@ -7,19 +6,19 @@ function Main() {
         <div className='min-h-screen border-b-2 border-black grid place-items-center'>
             <div className='container'>
                 <h1 className='text-black text-6xl uppecase font-bold lg:text-9xl text-right  '>
-                    Creative <span>desing,</span> & develop, <br/> russia.
+                    Creative <span>desing,</span> & develop, <br/> russia
                 </h1>
             </div>
         </div>
 
         <div id='about' className='min-h-screen border-b-2 border-black grid place-items-center '>
             <h3 className='container text-black text-6xl uppercase font-bold lg:text-9xl text-right'>
-                About me.
+                About me
             </h3>
 
             
             <p className='container text-xl'>
-            I am a third year student in information security at the Technological College of Omsk. I'm also self-taught, I'm very interested in frontend and backend development. I'm very interested in creating things related to technology and art. I enjoy learning on platforms that help me improve my skills, such as:
+            I am a third year student in information security at the Technological College of Omsk. I&apos;m also self-taught, I&apos;m very interested in frontend and backend development. I&apos;m very interested in creating things related to technology and art. I enjoy learning on platforms that help me improve my skills, such as:
             </p>
 
             <span className='container text-lg uppercase font-italic'>
