17d5bae73e168e87c2fcb7030e60763bc6e409a4
latushko1 <latushko1egor@gmail.com>
2022-09-21 19:47:07 +0600

    delete hover into scroller component

--- components/scroller.js
+++ components/scroller.js
@@ -4,7 +4,7 @@ import Marquee from 'react-fast-marquee'
 function Scroller({text}) {
   return (
     <section>
-        <Marquee gradient={false} pauseOnHover={true} speed={15} className='my-4 bg-primary border-y-2 border-secondary py-2 font-bold text-xl md:text-2xl text-secondary'>
+        <Marquee gradient={false} pauseOnHover={false} speed={15} className='my-4 bg-primary border-y-2 border-secondary py-2 font-bold text-xl md:text-2xl text-secondary'>
         {text}
         </Marquee>
     </section>
